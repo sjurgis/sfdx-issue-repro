93204e23999bcb62bf0228796709c304a02f72b6
heyoo <j.silute@gmail.com>
2021-01-22 13:20:43 +1300

    wip

--- create-org.sh
+++ create-org.sh
@@ -2,11 +2,5 @@
 set -e
 set -o pipefail
 
-CLIENT_ID=$1
-rm -f ./server.key
-cat > ./server.key
-
-sfdx auth:jwt:grant --clientid "$CLIENT_ID"  --jwtkeyfile ./server.key --username ci@devhub.decsond.com -a HubOrg
-sfdx force:org:create -v HubOrg -s -f ./admin/config/ci_scratch_template.json -d 1 -w 10 -a ciorg
-
-rm -f ./server.key
+#sfdx auth:jwt:grant --clientid 3MVG9SemV5D80oBeX0cnx.ekz5AjR9yfzePjZJlEK4nW1FfV7Z1EJm3cM_opgmAWOjXMF.jfIOuX.7KWGv6cf  --jwtkeyfile ./server.key --username dub.id-pmow@force.com -a HubOrg
+sfdx force:org:create -v HubOrg -s -f ./config/ci_scratch_template.json -d 1 -w 10 -a ciorg
